1511a6480879d2cc50379d0a2d21b64703721aff
MALB1993 <mostafaalaeddin570@gmail.com>
2024-08-27 19:58:26 -1200

    fixed products

--- src/pages/products/IndexProduct.js
+++ src/pages/products/IndexProduct.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProductsStart, fetchProductsSuccess, fetchProductsFailure } from '../../redux/products/ProductSlice';
+import { Card, Container, Row, Col, Button, Badge } from 'react-bootstrap';
+import Rating from 'react-rating';
 
 const IndexProduct = () => {
     const dispatch = useDispatch();
@@ -25,14 +27,32 @@ const IndexProduct = () => {
     if (status === 'failed') return <p>{error}</p>;
 
     return (
-        <div>
-            <h1>Products</h1>
-            <ul>
+        <Container>
+            <Row className='g-3'>
                 {products.map(product => (
-                    <li key={product.id}>{product.title	}</li>
+                    <Col md={3} sm={6} key={product.id}>
+                        <Card>
+                            <Card.Img variant="top" src={product.thumbnail} alt={product.name} />
+                            <Card.Body>
+                                <Card.Title className='fs-6 fw-bold'>{product.title} - { product.brand }</Card.Title>
+                                <Card.Text>
+                                    <p>{product.description}</p>
+                                    <p>Stock : {product.stock}</p>
+                                    <p>Price : ${product.price}</p>
+                                    {product.tags.map(tag => (
+                                        <Badge bg="dark" className='p-2 mx-1'>{tag}</Badge>
+                                    ))}
+                                    <div>
+                                        <Rating emptySymbol={<img src="/images/star-empty.png" className="icon" width={20} height={20}/>} fullSymbol={<img src="/images/star-full.png" className="icon" width={20} height={20}/>} initialRating={product.rating} readonly />
+                                    </div>
+                                </Card.Text>
+                                <Button variant="primary">Add to basket</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 ))}
-            </ul>
-        </div>
+            </Row>
+        </Container>
     );
 };
 
